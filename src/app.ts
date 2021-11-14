8972833439ac170d0a4b987bec406420e297eb16
TranHoangDat <tranhoangdat75@gmail.com>
2021-11-14 11:13:21 +0700

    Undo push new .env file

--- src/app.ts
+++ src/app.ts
@@ -21,49 +21,53 @@ const app = express();
 
 app.use("/public", express.static("public"));
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
+  express.urlencoded({
+    extended: true,
+  })
 );
 app.use(express.json({ limit: "1mb" }));
 app.use(cors());
 app.use((_: Request, res, next: NextFunction) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  next();
 });
 app.use("/", authRouter);
 app.use((req: Request, res: Response, next: NextFunction) => {
-    const authHeader = req.headers["authorization"];
-    if (authHeader?.startsWith("Bearer ")) {
-        const token = authHeader?.substr(7);
-        jwt.verify(token, process.env.TOKEN_SECRET as string, async (err: any, decoded: any) => {
-            if (err) {
-                res.sendStatus(401);
-            } else {
-                if (await getUserById(decoded.id)) {
-                    req.headers["userData"] = decoded;
-                    next();
-                } else {
-                    res.sendStatus(401);
-                }
-            }
-        });
-    } else {
-        res.sendStatus(401);
-    }
+  const authHeader = req.headers["authorization"];
+  if (authHeader?.startsWith("Bearer ")) {
+    const token = authHeader?.substr(7);
+    jwt.verify(
+      token,
+      process.env.TOKEN_SECRET as string,
+      async (err: any, decoded: any) => {
+        if (err) {
+          res.sendStatus(401);
+        } else {
+          if (await getUserById(decoded.id)) {
+            req.headers["userData"] = decoded;
+            next();
+          } else {
+            res.sendStatus(401);
+          }
+        }
+      }
+    );
+  } else {
+    res.sendStatus(401);
+  }
 });
 app.use("/users", userRouter);
 app.use("/classrooms", classroomRouter);
 if (!fs.existsSync("./public")) {
-    fs.mkdirSync("./public");
+  fs.mkdirSync("./public");
 }
 
 app.listen(process.env.PORT, () => {
-    sequelize
-        .sync({ alter: true })
-        .then((err: any) => {
-            console.log(`Server is listening at PORT ${process.env.PORT}`);
-        })
-        .catch(() => console.log("Cannot sync database!"));
+  sequelize
+    .sync({ alter: true })
+    .then((err: any) => {
+      console.log(`Server is listening at PORT ${process.env.PORT}`);
+    })
+    .catch(err => console.log(err.message));
 });
