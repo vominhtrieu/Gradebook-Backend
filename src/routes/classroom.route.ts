2ec8cd200aaaf5119ec4e50d9f4971d5470967e5
TranHoangDat <tranhoangdat75@gmail.com>
2021-11-24 17:42:26 +0700

    Add get classrooms with role teacher and role student

--- src/routes/classroom.route.ts
+++ src/routes/classroom.route.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import {
-    getClassroomDetailHandler,
-    getAllClassroomsHandler,
-    createClassroomHandler,
-    enrollClassroomHandler, sendInviteLinkHandler
+  getClassroomDetailHandler,
+  getAllClassroomsHandler,
+  createClassroomHandler,
+  enrollClassroomHandler,
+  sendInviteLinkHandler,
+  getClassroomsWithRoleStudentHandler,
+  getClassroomsWithRoleTeacherHandler,
 } from "../controllers/classroom.controller";
 
 const router = express.Router();
 
 router.post("/:id/enroll", enrollClassroomHandler);
 router.get("/:id", getClassroomDetailHandler);
+router.get("/role/student", getClassroomsWithRoleStudentHandler);
+router.get("/role/teacher", getClassroomsWithRoleTeacherHandler);
 router.get("/", getAllClassroomsHandler);
 router.post("/send-invitation-link", sendInviteLinkHandler);
 router.post("/", createClassroomHandler);
